d669cf9aadb969119c2033ead3c4ac5528998666
kejmiarz <kozlowskii@vp.pl>
2024-10-24 07:50:29 +0200

    Add files via upload

--- Untitled-1.php
+++ Untitled-1.php
@@ -4,64 +4,99 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>petla PHP</title>
+    <title>Document</title>
+    <style>
+        *{
+            font-style: 1.05pxe,;
+            font-family: verdana, Geneva, tahoma, sans-serif;
+
+        }
+        .czerwony{
+            color: red;
+            font-weight: bold;
+        }
+        .niebieski{
+            color: blue;
+            font-weight: bold;
+        }
+    </style>
+
 </head>
 <body>
     <?php
-    //phpinfo();
-    //
-    //
-    echo "<br>".rand(1,30)."<br>";
+    $tab1=['a','b','c','d','e']; //[45,67,78,34,56,78]
+    echo 'Tablica tab1 zawiera '.count($tab1)." elementw<br>";
+    foreach($tab1 as $a)
+    {
+        echo "$a ";
+    }
+    echo "<br>".$tab1[3]."<br>";
+    $tab2=[]; // [] operator tablicowy //array()
+    //$tan2=array();
+    $tab2[1]=100;
+    $tab2[50]=200.09;
+    $tab2[100]=150;
+    var_dump($tab2);
     echo "<br>";
-    $a=10;
-    echo ++$a."<br>";       // $x++ postinkermacja, ++$x preinkremacja
-                            // $a-- postinkremacja, --$a predekrementacja
-                            // zwiekszenie o 1, zmiejszenie o 1 wartosci zmiennej
-    echo $a."<br>";
-    for($i=1;$i<11;$i++)
-    {
-        if($i==5) continue;
-        echo "$i ";
-    }          
-    ?>
-    <hr>
-    <?php
-    $i=1;
-    do
-    {
-        echo "$i ";
-        $i++
-        if($i==7) break;
-    }while($i<11);
+    //echo $tab2[5]; //błąd !!!
+    foreach($tab2 as $klucz) echo "$klucz " ;
+echo " <br>";
+sort($tab2); // sortowanie tablicy rosnaco
+foreach($tab2 as $klucz) ecgo "$klucz";
+echo "<br>";
+rsort($tab2); //sortowanmoie tablicy malejaco
+foreach($tab2 as $klucz) echo "$klucz ";
+echo "<br>";
+$tab3=array();
+$tab3[]=rand(1,100);
+$tab3[5]=rand(1,100);
+$tab3[]=rand(1,100);
+echo "<br>";
+var_dump($tab3);
+echo "<br>";
+foreach ($tab3 as $klucz) echo "$klucz ";
+echo "<br>"
+echo min($tab3); //max //count //array_sum
+//$srednia=array_sum($tab3)/count($tab3)
+echo "<br>";
 
-    echo "<br>";
-    $i=1;
-    while($i<11)
-    {
-        echo "$i ";
-        $i++;
-    }
-    echo"<br>";
-    $tablica=['a','b','c','d'];
-    foreach($tablica as $klucz){
-        echo "$klucz ";
-    }
-    echo "<br>";
-    for($i=0;$i<sizeof($tablica);$i++)
-    {
-        echo $tablica[$i]." ";
-    }
-    echo "<br>";
-    $liczby[1]=100;
-    $liczby[20]=1200.9;
-    $liczby[300]=2;
-    //print_r($liczby);
-    var_dump($liczby);
-    echo "<br>";
-    for($i=0;$i<301;$i++)
-    {
-        if(isset($liczby[$i])) echo $liczby[$i]." ";
-    }
-    ?>
+//tablice asocjacyjne
+$tab4["imie"]="jan"; //klucz imie
+$tab4["nazwisko"]="Kowalski";
+echo $tab4 ["nazwisko"];
+echo "<br>";
+$dane = "ala,ola kowalska, ela";
+$tablica = explode(",",$dane);
+var_dump($tablica);
+echo "<br>";
+echo "<br>".$tablica[1]."<br>";
+$dane1 = implode(";;", $tablica);
+var_dump($dane1);
+echo "$dane1<br>";
+
+
+//przykladowe zadanie
+//wylosuj liczbe 100 liczb losowych w zakresie 10 do 50, policz min, max
+//wartosc oznacz kolorem czerwonym a wartosc max niebieksim
+for($i=0;$i<100;$i++)
+{
+    $tab[]=rand(10,50);
+}
+$min=min($tab);
+$max=max($tab);
+for(i=0;$i<100;i++)
+{
+    if($tab[$i]==$min) echo "<span class=czerwony>".$tab[$i]."</span>";
+    else if  if($tab[$i]==$max) echo "<span class=niebieski>".$tab[$i]."</span>";
+    else echo $tab[$i]." ";
+
+
+}
+echo "<br>";
+
+echo "min=$min";
+echo "<br>";
+echo "max=$max";
+?>
 </body>
 </html>
